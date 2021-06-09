e6e173f86a01dcd130f5f601b592ab9ffb5cce9d
Dominic <dominicantigua@gmail.com>
2021-06-09 14:33:25 +0800

    Move login animation in to a folder

--- login_animation/lib/routes/loginScreen.dart
+++ login_animation/lib/routes/loginScreen.dart
@@ -22,6 +22,7 @@ class _LoginScreenState extends State<LoginScreen>
         vsync: this, duration: Duration(seconds: 1, milliseconds: 500));
     _animation = Tween<double>(begin: 0.0, end: 1)
         .animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+    _controller.forward();
   }
 
   @override
@@ -38,65 +39,60 @@ class _LoginScreenState extends State<LoginScreen>
         child: SingleChildScrollView(
           child: Column(
             children: [
-              GestureDetector(
-                onTap: () {
-                  _controller.forward();
-                },
-                child: Container(
-                  height: SizeConfig.defaultSize * 45,
-                  decoration: const BoxDecoration(
-                    image: const DecorationImage(
-                        image: const AssetImage("assets/images/bg.png"),
-                        fit: BoxFit.fill),
-                  ),
-                  child: Center(
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          FadeTransition(
-                            opacity: _animation,
-                            child: Image(
-                              image: AssetImage("assets/images/banner.png"),
-                              height: SizeConfig.defaultSize * 7.5,
+              Container(
+                height: SizeConfig.defaultSize * 45,
+                decoration: const BoxDecoration(
+                  image: const DecorationImage(
+                      image: const AssetImage("assets/images/bg.png"),
+                      fit: BoxFit.fill),
+                ),
+                child: Center(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        FadeTransition(
+                          opacity: _animation,
+                          child: Image(
+                            image: AssetImage("assets/images/banner.png"),
+                            height: SizeConfig.defaultSize * 7.5,
+                          ),
+                        ),
+                        SizedBox(
+                          height: SizeConfig.defaultSize * 2.5,
+                        ),
+                        PartyAnimation(),
+                        SizedBox(
+                          height: SizeConfig.defaultSize * 1,
+                        ),
+                        FadeTransition(
+                          opacity: _animation,
+                          child: Text(
+                            "Welcome to budget++",
+                            style: TextStyle(
+                              fontSize: SizeConfig.defaultSize * 2,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
-                          SizedBox(
-                            height: SizeConfig.defaultSize * 2.5,
-                          ),
-                          PartyAnimation(),
-                          SizedBox(
-                            height: SizeConfig.defaultSize * 1,
-                          ),
-                          FadeTransition(
-                            opacity: _animation,
-                            child: Text(
-                              "Welcome to budget++",
-                              style: TextStyle(
-                                fontSize: SizeConfig.defaultSize * 2,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                          FadeTransition(
-                            opacity: _animation,
-                            child: Container(
-                              width: SizeConfig.defaultSize * 33,
-                              child: Wrap(
-                                children: [
-                                  Text(
-                                    "You just took the first step to get your money into shape. Let's get started!",
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      fontSize: SizeConfig.defaultSize * 1.3,
-                                      color: Colors.black,
-                                    ),
+                        ),
+                        FadeTransition(
+                          opacity: _animation,
+                          child: Container(
+                            width: SizeConfig.defaultSize * 33,
+                            child: Wrap(
+                              children: [
+                                Text(
+                                  "You just took the first step to get your money into shape. Let's get started!",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: SizeConfig.defaultSize * 1.3,
+                                    color: Colors.black,
                                   ),
-                                ],
-                              ),
+                                ),
+                              ],
                             ),
                           ),
-                        ]),
-                  ),
+                        ),
+                      ]),
                 ),
               ),
               FadeTransition(
